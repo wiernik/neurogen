a20cce4d82a326d7688fa6bfb5e3674312da9d5e
MarvinLvn <marvinlavechin@gmail.com>
2025-06-02 17:59:29 -0400

    Update instructions VCM

--- docs/vcm.md
+++ docs/vcm.md
@@ -1,1 +1,41 @@
-TO WRITE
+We will compare two algorithms: 
+- VCMNet from Futaisi et al. (2019)
+- Meg's algorithm from Suresh et al. (2025)
+
+To do so, we will work with our 25 hours of audio annotated by a single human expert (5 hours per diagnostic group).
+Both classifiers will be applied exclusively on KCHI utterances.
+
+# A. Applying [VCMNet](https://github.com/LAAC-LSCP/vcm)
+
+1. First, we need to extract KCHI utterances from annotation files:
+
+```sh
+python utils/extract_rttm_KCHI.py --data data/L3_HIPAA_LENA_cleaned/annotations/eaf/an1/converted --output data/L3_HIPAA_LENA_cleaned/annotations/eaf/an1/rttm
+```
+
+This will create a folder containing .rttm files with KCHI utterances such as found by the human annotator. 
+
+2. We can then apply VCM following the instructions in this [git](https://github.com/LAAC-LSCP/vcm).
+
+# B. Applying [Meg's algorithm](https://github.com/spoglab-stanford/w2v2-pro-sm/tree/main/speechbrain/recipes/W2V2-LL4300-Pro-SM)
+
+1. Install the dependencies:
+
+```shell
+git clone https://github.com/spoglab-stanford/w2v2-pro-sm
+cd w2v2-pro-sm/speechbrain/recipes/W2V2-LL4300-Pro-SM
+conda create -n megvcm python=3.10 -y
+pip install -r requirements.txt
+```
+
+2. a
+
+# C. 
+
+# References 
+
+```
+Al Futaisi, N., Zhang, Z., Cristia, A., Warlaumont, A., & Schuller, B. (2019). VCMNet: Weakly supervised learning for automatic infant vocalisation maturity analysis. International Conference on Multimodal Interaction (pp. 205-209).
+Zhang, T., Suresh, A., Warlaumont, A., Hitczenko, K., Cristia, A., Cychosz, M. (2025) Employing self-supervised learning models for cross-linguistic child speech maturity classification. In review in Intersspech.
+```
+
---
+++ utils/extract_rttm_KCHI.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+Convert CSV files in a folder to RTTM format.
+"""
+
+import argparse
+import csv
+import os
+from pathlib import Path
+import pandas as pd
+from tqdm import tqdm
+
+def read_csv(path):
+    data = pd.read_csv(path, sep=',')
+    data['filename'] = path.stem
+    file_onset = int(path.stem.split('_')[-2])
+    data['segment_onset'] = data['segment_onset'] - file_onset
+    data['segment_offset'] = data['segment_offset'] - file_onset
+    return data
+
+def csv2rttm(csv_data):
+    cols = csv_data.columns
+    assert 'segment_onset' in cols and 'segment_offset' in cols and 'speaker_type' in cols
+    rttm = pd.DataFrame(index=csv_data.index)
+    rttm['first'] = 'SPEAKER'
+    rttm['second'] = csv_data['filename']
+    rttm['third'] = 1
+    rttm['fourth'] = csv_data['segment_onset'] / 1000
+    rttm['fifth'] = (csv_data['segment_offset'] - csv_data['segment_onset'])/1000
+    rttm['sixth'] = '<NA>'
+    rttm['seventh'] = '<NA>'
+    rttm['eigth'] = csv_data['speaker_type']
+    rttm['ninth'] ='<NA>'
+    rttm['tenth'] = '<NA>'
+
+    # Replace ACLEW convention to VTC convention
+    speaker_map = {'CHI': 'KCHI',
+                   'OCH': 'CHI',
+                   'MAL': 'MAL',
+                   'FEM': 'FEM'}
+    rttm['eigth'] = rttm['eigth'].map(lambda x: speaker_map[x])
+    rttm = rttm[rttm['eigth'] == 'KCHI']
+    return rttm
+
+def write_rttm(rttm_data, rttm_path):
+    rttm_data.to_csv(rttm_path, sep=' ', index=False, header=False)
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert CSV files to RTTM format")
+    parser.add_argument('--data', required=True, help='Path to the folder containing human annotation .csv files.')
+    parser.add_argument('--output', required=True, help='Path to the output folder where rttm will be generated.')
+    args = parser.parse_args()
+
+    if args.data == args.output:
+        raise ValueError("--output cannot be the same as --data.")
+
+    args.data = Path(args.data)
+    args.output = Path(args.output)
+    args.output.mkdir(parents=True, exist_ok=True)
+
+    csv_files = list(args.data.glob('*.csv'))
+    if len(csv_files) == 0:
+        raise ValueError(f"Cannot find .csv files in {args.data}")
+
+    for csv_file in tqdm(csv_files):
+        output_file = args.output / (csv_file.stem + '.rttm')
+        csv_data = read_csv(csv_file)
+        rttm_data = csv2rttm(csv_data)
+        write_rttm(rttm_data, output_file)
+
+if __name__ == '__main__':
+    main()
