ec37d52a8165c64f6641f724c38ee2b2744fbe21
wiernik <151029057+wiernik@users.noreply.github.com>
2025-07-05 23:20:19 -0400

    Update pandas_matrix.py
    removed redundant checks for NaN values after merge 'how' changed from 'outer' -> 'inner'

--- analysis/pandas_matrix.py
+++ analysis/pandas_matrix.py
@@ -19,7 +19,7 @@ meg_files = list(base_dir.glob('annotations/meg_vcm/converted_from_human_timesta
 def files_to_dataframe(files, source):
     dfs = []
     for file_path in files:
-        df = pd.read_csv(file_path, dtype={'segment_onset': int, 'segment_offset': int}) # make sure segments show up as ints
+        df = pd.read_csv(file_path)
 
         df = df[(df['speaker_type'] == 'CHI') &                         # filter for speaker type
                 (df['vcm_type'] != 'U') &                               # filter out unsure labels
@@ -37,14 +37,14 @@ human_files = files_to_dataframe(human_files, 'human_annotations')
 vcm_files = files_to_dataframe(vcm_files, 'VCMNet')
 # meg_files = files_to_dataframe(meg_files, 'meg_classifier')
 
-csv_files = human_files.merge(vcm_files, on=['segment_onset', 'segment_offset', 'filename'], # add suffixes for clarity
-                             suffixes=('_human', '_vcmnet'), how='inner') 
+csv_files = human_files.merge(vcm_files, on=['segment_onset', 'segment_offset', 'filename'], 
+                             suffixes=('_human', '_vcmnet'), how='inner')  # add suffixes for clarity
 
 # below for when adding in meg_classifier
-# csv_files = human_files.merge(vcm_files, on=['segment_onset', 'segment_offset', 'filename'], # add suffixes for clarity
+# csv_files = human_files.merge(vcm_files, on=['segment_onset', 'segment_offset', 'filename'], 
 #                              suffixes=('_human', '_vcmnet'))\
 #                        .merge(meg_files, on=['segment_onset', 'segment_offset', 'filename'], 
-#                              suffixes=('', '_meg'), how='outer')
+#                              suffixes=('', '_meg'), how='inner')
 
 csv_files = csv_files[['filename', 'segment_onset', 'segment_offset', 
                        'vcm_type_human', 'vcm_type_vcmnet']]
@@ -83,14 +83,14 @@ csv_files = csv_files[['filename', 'segment_onset', 'segment_offset',
 
 # ####################### confusion matrix #######################
 
-labels = sorted(list(set(csv_files['vcm_type_human'].dropna()) | 
-                     set(csv_files['vcm_type_vcmnet'].dropna())))
+labels = sorted(list(set(csv_files['vcm_type_human']) | 
+                     set(csv_files['vcm_type_vcmnet'])))
 
 VCMNet_human_confusion_matrix = pd.DataFrame(0, index=labels, columns=labels)
 
 for human_label, vcm_label in zip(csv_files['vcm_type_human'], csv_files['vcm_type_vcmnet']):                          
-    if pd.notna(human_label) and pd.notna(vcm_label):
-        if human_label in labels and vcm_label in labels:
+    if pd.notna(human_label) and pd.notna(vcm_label):                        # robustness checks but maybe unnecessary?
+        if human_label in labels and vcm_label in labels:                    # robustness checks but maybe unnecessary?
             VCMNet_human_confusion_matrix.loc[human_label, vcm_label] += 1              
 
 # print(VCMNet_human_confusion_matrix)
